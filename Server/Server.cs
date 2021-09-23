bbdba4ecd90f42f3c3c44b48e3fa989cbd10200b
pitni <pitni@PITNICHYK>
2021-09-23 22:40:16 +0300

    server 1 class

--- Server/Server.cs
+++ Server/Server.cs
@@ -88,7 +88,6 @@ namespace Server
             }
             return true;
         }
-
         static string GetFileName(Socket clientSoc)
         {
 
@@ -103,124 +102,74 @@ namespace Server
 
             return stringBuilder.ToString();
         }
-
-
         static string GetFileSize(Socket clientSoc)
         {
-
             StringBuilder stringBuilder = new StringBuilder();
             byte[] data = new byte[256];
-
             do
             {
                 bytes = clientSoc.Receive(data);
                 stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
             } while (socketClient.Available > 0);
-
             return stringBuilder.ToString();
         }
 
         static StringBuilder GetPosition_X(Socket clientSoc)
         {
-
             StringBuilder stringBuilder = new StringBuilder();
             byte[] data = new byte[256];
-
             do
             {
                 bytes = clientSoc.Receive(data);
                 stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
             } while (socketClient.Available > 0);
-
             return stringBuilder;
-
         }
-
-
         static int CheckWin(int[,] pole, int win)
         {
-
             if (pole[0, 0] == 1 && pole[0, 1] == 1 && pole[0, 2] == 1)
-            {
-
                 win = 1;
 
-            }
             else if (pole[1, 0] == 1 && pole[1, 1] == 1 && pole[1, 2] == 1)
-            {
-
                 win = 1;
 
-            }
             else if (pole[2, 0] == 1 && pole[2, 1] == 1 && pole[2, 2] == 1)
-            {
-
                 win = 1;
 
-            }
             else if (pole[0, 0] == 0 && pole[0, 1] == 0 && pole[0, 2] == 0)
-            {
-
                 win = 2;
 
-            }
             else if (pole[1, 0] == 0 && pole[1, 1] == 0 && pole[1, 2] == 0)
-            {
-
                 win = 2;
 
-            }
             else if (pole[2, 0] == 0 && pole[2, 1] == 0 && pole[2, 2] == 0)
-            {
-
                 win = 2;
 
-            }
             else if (pole[0, 0] == 0 && pole[1, 1] == 0 && pole[2, 2] == 0)
-            {
-
                 win = 2;
 
-            }
             else if (pole[0, 2] == 0 && pole[1, 1] == 0 && pole[2, 0] == 0)
-            {
-
                 win = 2;
 
-            }
             else if (pole[0, 0] == 1 && pole[1, 1] == 1 && pole[2, 2] == 1)
-            {
-
                 win = 1;
 
-            }
             else if (pole[0, 2] == 1 && pole[1, 1] == 1 && pole[2, 0] == 1)
-            {
-
                 win = 1;
 
-            }
             else
-            {
                 win = 0;
-            }
 
             return win;
         }
 
         static void DrawField(char[,] field)
         {
-
             Console.WriteLine($"{field[0, 0]} | {field[0, 1]} | {field[0, 2]}");
             Console.WriteLine("---------");
             Console.WriteLine($"{field[1, 0]} | {field[1, 1]} | {field[1, 2]}");
             Console.WriteLine("---------");
             Console.WriteLine($"{field[2, 0]} | {field[2, 1]} | {field[2, 2]}");
-
-
         }
-
     }
-
-
 }
